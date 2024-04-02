e5a0ae181c1376b5a1813a240f2bdeb04c6f97a0
Mariam-bobby <90889090+Mariam-bobby@users.noreply.github.com>
2024-04-02 19:36:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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";
+const API_KEY = "Enter your API-KEY";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
